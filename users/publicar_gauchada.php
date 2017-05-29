889caea1d2f7b059bb28ad2c44ed9fc596bc108a
greatCharles <menendez_carlos@yahoo.com.ar>
2017-05-29 03:14:28 +0200

    Add files via upload

--- users/publicar_gauchada.php
+++ users/publicar_gauchada.php
@@ -10,24 +10,65 @@ ini_set("allow_url_fopen", 1);
 
 <?php
 
+//conexion a la bd y categorias
 $conex= mysqli_connect('localhost', 'root', '', 'db');
-
-$consulta= "SELECT nombre FROM categorias";
-
+$consulta= "SELECT nombre FROM categoria";
 $result= mysqli_query($conex, $consulta);
 
-?>
+//Esto es todo lo que se hace cuando se envía el formulario
+$query_locali = "SELECT localidad, provincia FROM localidades";
+$result_locali = mysqli_query($conex, $query_locali);
 
+// if(isset($_POST['titulo'])){
+// 	$titulo= $_POST['titulo'];	
+// }
+
+// if(isset($_POST['descripcion'])){
+// 	if(!empty($_POST['descripcion'])){
+// 		$descripcion= $_POST['descripcion'];	
+// 	}
+// 	else{
+// 	echo 'Debe completar el campo descripcion';
+// 	}
+// }
+
+// if(isset($_POST['ubicacion'])){
+// 	$ubicacion= $_POST['ubicacion'];
+// 	$ubicacion_explode= explode(' - ', $ubicacion);
+// 	$localidad= $ubicacion_explode[0];
+// 	$provincia= $ubicacion_explode[1];
+// }
+// else{
+// 	echo 'Debe completar el campo ubicación';
+// }
+
+// if(isset($_POST['titulo'])){
+// 	$titulo= $_POST['titulo'];	
+// }
+
+// if(isset($_POST['categoria'])){
+// 	$categoria= $_POST['categoria'];
+// }
+
+// if(isset($_POST['fecha'])){
+// 	$fecha_exacta= $_POST['fecha'];	
+// }else{
+// 	$fecha_exacta= NULL;
+// }
+
+// $id_usuario= $user->data()->id;
+
+// $consulta_categoria= 'SELECT id_categoria FROM categoria WHERE nombre= $categoria';
+// $id_categoria= mysqli_query($conex, $consulta_categoria);
+// $consulta_insertar= 'INSERT INTO gauchada(titulo, descripcion, provincia, ciudad, id_usuario, id_categoria, fecha_exacta) VALUES($titulo, $descripcion, $provincia, $localidad, $id_usuario, $id_categoria, $fecha_exacta)';
+// mysqli_query($conex, $consulta_insertar);
+
+?>
 
 <?php header('X-Frame-Options: DENY'); ?>
 <div id="page-wrapper">
 <div class="container">
-
-
 <?php require 'views/_publicar_gauchada.php';?>
-
-
-
 </div>
 </div>
 
