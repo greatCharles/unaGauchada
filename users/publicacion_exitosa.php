e6993d2faa22d588271cd0a2b940ba82a4b577af
greatCharles <menendez_carlos@yahoo.com.ar>
2017-05-29 23:24:07 -0300

    Add files via upload

---
+++ users/publicacion_exitosa.php
@@ -0,0 +1,15 @@
+<?php require_once 'init.php'; ?>
+<?php require_once $abs_us_root.$us_url_root.'users/includes/header.php'; ?>
+<?php require_once $abs_us_root.$us_url_root.'users/includes/navigation.php'; ?>
+<div id="page-wrapper">
+<div class="container">
+<?php require 'views/_publicacion_exitosa.php';?>
+</div>
+</div>
+<!-- Content Ends Here -->
+<!-- footers -->
+<?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
+
+<!-- Place any per-page javascript here -->
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; // currently just the closing /body and /html ?>
