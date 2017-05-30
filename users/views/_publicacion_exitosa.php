8300cd1db150ff2df6049ec39082f75c175b6ac2
greatCharles <menendez_carlos@yahoo.com.ar>
2017-05-29 23:24:34 -0300

    Add files via upload

---
+++ users/views/_publicacion_exitosa.php
@@ -0,0 +1,8 @@
+  <div class="row">
+  <div class="col-xs-12">
+  <div class="jumbotron text-center">
+  <h1>Felicitaciones!</h1>
+  <p>Su gauchada ha sido publicada exitosamente.</p>
+  </div>
+  </div>
+  </div>
