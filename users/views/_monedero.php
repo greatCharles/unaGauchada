73d2f8154f132b834f9d248ca614d24d81bb6825
greatCharles <menendez_carlos@yahoo.com.ar>
2017-06-01 14:38:02 -0300

    Add files via upload

---
+++ users/views/_monedero.php
@@ -0,0 +1,5 @@
+<h1>Monedas disponibles</h1><br><br>
+<?php //acá tiene que ir la consulta que solicita los creditos que tenga el user ?>
+
+<?php //bien a la derecha habría que tirar el botón que diga comprar monedas y que direccione al comprar créditos,
+//cambiando el nombre por monedas obvio. ?>
